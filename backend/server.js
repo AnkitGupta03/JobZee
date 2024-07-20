a17bbe321d2ffcca9f5c36bd2ae32cfb99627028
AnkitGupta03 <ankit03102021@gmail.com>
2024-07-20 16:28:36 +0530

    allowed access to cors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
+const express = require('express');
+const app = express();
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://job-zee-rust.vercel.app");
+  next();
 });
 
 app.listen(process.env.PORT, () => {
