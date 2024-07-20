3eccff03fb499119831e098076342b219932381f
AnkitGupta03 <ankit03102021@gmail.com>
2024-07-20 16:10:25 +0530

    updated dotenv file

--- frontend/src/components/Application/MyApplications.jsx
+++ frontend/src/components/Application/MyApplications.jsx
@@ -18,17 +18,23 @@ const MyApplications = () => {
     try {
       if (user && user.role === "Employer") {
         axios
-          .get("http://localhost:4000/api/v1/application/employer/getall", {
-            withCredentials: true,
-          })
+          .get(
+            "import.meta.env.BACKEND_URL/api/v1/application/employer/getall",
+            {
+              withCredentials: true,
+            }
+          )
           .then((res) => {
             setApplications(res.data.applications);
           });
       } else {
         axios
-          .get("http://localhost:4000/api/v1/application/jobseeker/getall", {
-            withCredentials: true,
-          })
+          .get(
+            "import.meta.env.BACKEND_URL/api/v1/application/jobseeker/getall",
+            {
+              withCredentials: true,
+            }
+          )
           .then((res) => {
             setApplications(res.data.applications);
           });
@@ -45,7 +51,7 @@ const MyApplications = () => {
   const deleteApplication = (id) => {
     try {
       axios
-        .delete(`http://localhost:4000/api/v1/application/delete/${id}`, {
+        .delete(`import.meta.env.BACKEND_URL/api/v1/application/delete/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
